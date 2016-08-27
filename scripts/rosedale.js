57f2c8f5c3ba541c3a5ae7318fef6263e196b194
crccheck <c@crccheck.com>
2016-08-27 15:10:11 -0500

    store data in one big object

--- scripts/rosedale.js
+++ scripts/rosedale.js
@@ -12,14 +12,17 @@ i18n.configure({
   defaultLocale: 'en_US',
   directory: path.join(__dirname, '../locales')
 })
-const NEW = 'new'
-const REG_CHECK = 'reg_check'
-const REG_CHECK_Y = 'reg_check_y'
-const REG_CHECK_N = 'reg_check_n'
 
 const DEFAULT_STATE = {
-  state: NEW,
+  state: 'new',
   lang: 'en'
+}
+const key = 'tt:users'
+
+// i18n expects a slightly different format
+function _ (input) {
+  // return i18n.__(input)[1]
+  return '`' + input + '` ' + i18n.__(input)[1]  // DEBUG
 }
 
 // should I be using a state machine library? ugh, then I'd have to search for a state machine library
@@ -35,17 +38,11 @@ function isNo (text) {
 function sendResponse (robot, res) {
   // console.log(res.message.user.id)  // name
   const userKey = res.message.user.id  // Phone number
-  const key = `tt:users:${userKey}`
-  const userInfo = robot.brain.get(key) || Object.assign({}, DEFAULT_STATE)
+  const data = robot.brain.get(key) || {}
+  const userInfo = data[userKey] || Object.assign({}, DEFAULT_STATE)
   robot.logger.debug(userInfo)
 
   const text = res.message.text.replace('rosedale ', '')
-
-  // i18n expects a slightly different format
-  function _ (input) {
-    // return i18n.__(input)[1]
-    return input + ' ' + i18n.__(input)[1]  // DEBUG
-  }
 
   if (text === 'reset') {
     Object.assign(userInfo, DEFAULT_STATE)
@@ -56,29 +53,36 @@ function sendResponse (robot, res) {
   if (text === 'day') {
     userInfo.state = 'day'
   }
+  if (text.toLowerCase() === 'stop') {
+    delete data[userKey]
+    robot.brain.set(key, data)
+    res.reply(_('stop'))
+    return
+  }
+
   res.reply('```' + JSON.stringify(userInfo, undefined, 2) + '```') // DEBUG
 
-  if (userInfo.state === NEW) {
-    userInfo.state = REG_CHECK
-    robot.brain.set(key, userInfo)
+  if (userInfo.state === 'new') {
+    userInfo.state = 'reg_check'
+    robot.brain.set(key, data)
     res.reply(_('new reg_check'))
     return
   }
 
-  if (userInfo.state === REG_CHECK) {
+  if (userInfo.state === 'reg_check') {
     if (isYes(text)) {
       userInfo.state = 'reg_check_y'
-      robot.brain.set(key, userInfo)
+      robot.brain.set(key, data)
       res.reply(_('reg_check reg_check_y'))
       return
     } else if (isNo(text)) {
       userInfo.state = 'reg_check_n'
-      robot.brain.set(key, userInfo)
+      robot.brain.set(key, data)
       res.reply(_('reg_check reg_check_n'))
       return
     } else {
       userInfo.state = 'reg_check_?'
-      robot.brain.set(key, userInfo)
+      robot.brain.set(key, data)
       res.reply(_('reg_check reg_check_?'))
       return
     }
@@ -86,7 +90,7 @@ function sendResponse (robot, res) {
 
   if (userInfo.state === 'early') {
     userInfo.state = 'early_notified'
-    robot.brain.set(key, userInfo)
+    robot.brain.set(key, data)
     res.reply(_('early'))
     return
   }
@@ -94,17 +98,17 @@ function sendResponse (robot, res) {
   if (userInfo.state === 'early_notified') {
     if (isYes(text)) {
       userInfo.state = 'early_y'
-      robot.brain.set(key, userInfo)
+      robot.brain.set(key, data)
       res.reply(_('early_y'))
       return
     } else if (isNo(text)) {
       userInfo.state = 'early_n'
-      robot.brain.set(key, userInfo)
+      robot.brain.set(key, data)
       res.reply(_('early_n'))
       return
     } else {
       userInfo.state = 'early_?'
-      robot.brain.set(key, userInfo)
+      robot.brain.set(key, data)
       res.reply(_('early_?'))
       return
     }
@@ -113,17 +117,17 @@ function sendResponse (robot, res) {
   if (userInfo.state === 'early_?') {
     if (text === '1') {
       userInfo.state = 'early_?_1'
-      robot.brain.set(key, userInfo)
+      robot.brain.set(key, data)
       res.reply(_('early_?_1'))
       return
     } else if (text === '2') {
       userInfo.state = 'early_?_2'
-      robot.brain.set(key, userInfo)
+      robot.brain.set(key, data)
       res.reply(_('early_?_2'))
       return
     } else {
       userInfo.state = 'early_?_3'
-      robot.brain.set(key, userInfo)
+      robot.brain.set(key, data)
       res.reply(_('early_?_3'))
       return
     }
@@ -132,17 +136,17 @@ function sendResponse (robot, res) {
   if (userInfo.state === 'early_?_3') {
     if (isYes(text)) {
       userInfo.state = 'early_?_3_y'
-      robot.brain.set(key, userInfo)
+      robot.brain.set(key, data)
       res.reply(_('early_?_3_y'))
       return
     } else if (isNo(text)) {
       userInfo.state = 'early_?_3_n'
-      robot.brain.set(key, userInfo)
+      robot.brain.set(key, data)
       res.reply(_('early_?_3_n'))
       return
     } else {
       userInfo.state = 'early_?_3_?'
-      robot.brain.set(key, userInfo)
+      robot.brain.set(key, data)
       res.reply(_('early_?_3_?'))
       return
     }
@@ -150,27 +154,31 @@ function sendResponse (robot, res) {
 
   if (userInfo.state === 'election_morning') {
     userInfo.state = 'election_morning_notified'
-    robot.brain.set(key, userInfo)
+    robot.brain.set(key, data)
     res.reply(_('election_morning'))
     return
   }
 
   if (userInfo.state === 'election_day') {
     userInfo.state = 'election_day_notified'
-    robot.brain.set(key, userInfo)
+    robot.brain.set(key, data)
     res.reply(_('election_day'))
     return
   }
 
   if (userInfo.state === 'election_over') {
     userInfo.state = 'election_over_notified'
-    robot.brain.set(key, userInfo)
+    robot.brain.set(key, data)
     res.reply(_('election_over'))
     return
   }
 }
 
 module.exports = (robot) => {
+  robot.respond(/./i, (res) => {
+    sendResponse(robot, res)
+  })
+
   robot.respond(/./i, (res) => {
     sendResponse(robot, res)
   })
