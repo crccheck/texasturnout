94ecdf032e037ce63774b5833969af034dfb928e
crccheck <c@crccheck.com>
2016-08-27 11:30:01 -0500

    stub in translation logic

--- scripts/rosedale.js
+++ scripts/rosedale.js
@@ -1,10 +1,14 @@
 // Description:
 //   TT & SND Texas Turnout sms voting
 
+// const Gettext = require("node-gettext")
+// const gt = new Gettext()
+// const _ = gt.gettext  // TODO
+const _ = (str) => str
 
 module.exports = (robot) => {
   // where do i go to vote
   robot.hear(/where.+go.+vote/i, (res) => {
-    res.reply('go to the polls')
+    res.reply(_('go to the polls'))
   })
 }
